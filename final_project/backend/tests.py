df3ee4b49cbadb9f5775b9a4b41b845c583df37d
Fabkar <fab_862@hotmail.com>
2021-04-24 14:45:29 -0500

    building the first unitest to each class and methods

--- final_project/backend/tests.py
+++ final_project/backend/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from backend.models import Client
 
 # Create your tests here.
+class Test_user(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.dummy = Client.objects.create(gov_id="F0008G",
+                                        first_name="Fabian",
+                                        last_name="Carmona",
+                                        email="test@gmail.com",
+                                        company="retailer company",
+                                        )
+
+    def test_allusers(self):
+        users = Client.objects.all()
+        self.assertEquals(users.count(), 1)
+
+    def test_quiery_specific(self):
+        query = Client.objects.filter(first_name="Fabian")
+        print(query)
+        self.assertEqual(query.first_name,"Fabian")
