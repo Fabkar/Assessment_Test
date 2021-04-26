70a7336b0fd05bb40d3317ab49195f2429309778
Fabkar <fab_862@hotmail.com>
2021-04-26 16:07:37 -0500

    update file with the models

--- final_project/backend/admin.py
+++ final_project/backend/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from backend.models import Client
+from backend.models import Client, Order, Shipping, Payment
 
 
 # Register your models here.
 admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Shipping)
+admin.site.register(Payment)
