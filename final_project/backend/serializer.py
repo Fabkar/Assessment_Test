6e05ca85759ed0e1830eeb137d52cf8296aece06
Fabkar <fab_862@hotmail.com>
2021-04-27 08:29:35 -0500

    Class serializer to each model

--- final_project/backend/serializer.py
+++ final_project/backend/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from backend.models import Client, Order
+from backend.models import Client, Order, Payment, Shipping
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
+        fields = "__all__"
+class ShippingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shipping
+        fields = "__all__"
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = "__all__"
